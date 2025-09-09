7a9b00f40727f4a6de8ad878662e3f448ee1d9d3
canwdev <9585126+canwdev@users.noreply.github.com>
2025-09-09 18:26:49 +0800

    update

---
+++ src/routes/files/types.ts
@@ -0,0 +1,33 @@
+export interface IEntry {
+  name: string
+  ext: string
+  isDirectory: boolean
+  hidden: boolean
+  lastModified: number
+  birthtime: number
+  size: number | null
+  error: string | null
+  // stat?: any
+  // mimeType?: string
+}
+
+export interface IDrive {
+  label: string
+  path: string
+  free?: number
+  total?: number
+}
+
+export enum SortType {
+  default = 'default',
+  name = 'name',
+  nameDesc = 'nameDesc',
+  size = 'size',
+  sizeDesc = 'sizeDesc',
+  extension = 'extension',
+  extensionDesc = 'extensionDesc',
+  lastModified = 'lastModified',
+  lastModifiedDesc = 'lastModifiedDesc',
+  birthTime = 'birthTime',
+  birthTimeDesc = 'birthTimeDesc',
+}
