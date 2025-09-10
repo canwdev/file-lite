1cfed472be1c783bc8067c93b38c80b672343619
canwdev <9585126+canwdev@users.noreply.github.com>
2025-09-10 11:20:03 +0800

    update: 下载文件/文件夹

--- src/routes/files/index.ts
+++ src/routes/files/index.ts
@@ -3,6 +3,7 @@ import {
   copyPastePath,
   createDirectory,
   deletePath,
+  downloadPath,
   getDrivers,
   getFiles,
   getFileStream,
@@ -13,10 +14,12 @@ const filesRouter = express.Router()
 
 filesRouter.get('/drivers', getDrivers)
 filesRouter.get('/list', getFiles)
-filesRouter.get('/stream', getFileStream)
 filesRouter.post('/create-dir', createDirectory)
 filesRouter.post('/rename', renamePath)
 filesRouter.post('/copy-paste', copyPastePath)
 filesRouter.post('/delete', deletePath)
+
+filesRouter.get('/stream', getFileStream)
+filesRouter.get('/download', downloadPath)
 
 export {filesRouter}
