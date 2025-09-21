89d60f5647d76e13d5d9b9434742a239f3d24126
canwdev <dev@example.com>
2025-09-21 11:46:36 +0800

    update: fix limiter

--- src/middlewares/limiter.ts
+++ src/middlewares/limiter.ts
@@ -1,5 +1,11 @@
 import rateLimit from 'express-rate-limit'
 
+const skipPathMap = {
+  '/files/stream': true,
+  '/files/download': true,
+  '/files/upload-file': true,
+}
+
 // https://express-rate-limit.mintlify.app/reference/configuration
 export const limiter = rateLimit({
   // 限制时间窗口：1 分钟 (x * 60 秒 * 1000 毫秒)
@@ -7,4 +13,7 @@ export const limiter = rateLimit({
   // 每个 IP 在一个窗口期内最多允许 x 次请求
   limit: 1000,
   message: {message: 'Too many requests, please try again later.'},
+  skip: (req, res) => {
+    return skipPathMap[req.path] || false
+  },
 })
