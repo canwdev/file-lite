f828c656423ecff97b18c8c4c20fadac9931ee1c
canwdev <c2097@live.com>
2025-09-22 09:42:22 +0800

    update: dark theme

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
-import { ElMessage, ElMessageBox } from 'element-plus'
-import { authToken } from './store'
+import {ElMessage, ElMessageBox} from 'element-plus'
+import {authToken} from './store'
+import {useGlobalTheme} from '@/hooks/use-global-theme.ts'
 window.$message = ElMessage
 window.$dialog = ElMessageBox
 
 const router = useRouter()
 
+useGlobalTheme()
 window.$logout = () => {
   authToken.value = ''
-  router.push({ name: 'LoginView' })
+  router.push({name: 'LoginView'})
 }
 </script>
 
