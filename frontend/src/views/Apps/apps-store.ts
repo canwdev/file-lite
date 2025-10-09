d3a68e8a65c7fd339bf930d462e2614f1d243a33
canwdev <c2097@live.com>
2025-10-09 18:32:12 +0800

    update: confirm dialogs

--- frontend/src/views/Apps/apps-store.ts
+++ frontend/src/views/Apps/apps-store.ts
@@ -4,11 +4,13 @@ import { OpenWithEnum } from './apps'
 interface AppsStore {
   isShowApp: boolean
   appName: OpenWithEnum
+  appTitle: string
   appParams: null | AppParams
 }
 
 export const appsStoreState = reactive<AppsStore>({
   isShowApp: false,
   appName: OpenWithEnum.TextEditor,
+  appTitle: '',
   appParams: null,
 })
