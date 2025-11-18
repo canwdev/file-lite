65b97a91e1f3156fa061b3575caf283f885b65c9
canwdev <canwdev@users.noreply.github.com>
2025-11-18 11:49:45 +0800

    gofmt

--- backend-go/routes/index.go
+++ backend-go/routes/index.go
@@ -1,21 +1,20 @@
 package routes
 
 import (
-    "net/http"
-    "time"
+	"net/http"
+	"time"
 
-    "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4"
 
-    "file-lite-go/config"
-    "file-lite-go/middlewares"
+	"file-lite-go/config"
+	"file-lite-go/middlewares"
 )
 
 func Register(api *echo.Group) {
-    api.GET("/", func(c echo.Context) error {
-        return c.JSON(http.StatusOK, map[string]any{"name": config.PkgName, "version": config.Version, "timestamp": time.Now().UnixMilli()})
-    })
-    files := api.Group("/files")
-    files.Use(middlewares.AuthMiddleware)
-    registerFiles(files)
+	api.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]any{"name": config.PkgName, "version": config.Version, "timestamp": time.Now().UnixMilli()})
+	})
+	files := api.Group("/files")
+	files.Use(middlewares.AuthMiddleware)
+	registerFiles(files)
 }
-
--- backend-go/utils/log.go
+++ backend-go/utils/log.go
@@ -1,18 +1,18 @@
 package utils
 
 import (
-    "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4"
 )
 
 func LogErrorsOnly() echo.MiddlewareFunc {
-    return func(next echo.HandlerFunc) echo.HandlerFunc {
-        return func(c echo.Context) error {
-            err := next(c)
-            if err != nil {
-                c.Logger().Error(err)
-                return err
-            }
-            return nil
-        }
-    }
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			err := next(c)
+			if err != nil {
+				c.Logger().Error(err)
+				return err
+			}
+			return nil
+		}
+	}
 }
