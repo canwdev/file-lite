6a34630fb228ad55c39f10d88f335c7d17318b40
canwdev <canwdev@users.noreply.github.com>
2026-08-14 14:25:57 +0800

    feat: enhance HtmlViewer

--- frontend/src/views/Apps/HtmlViewer.vue
+++ frontend/src/views/Apps/HtmlViewer.vue
@@ -1,7 +1,6 @@
 <script lang="ts" setup>
 import type { AppParams } from '@/views/Apps/apps.ts'
 import { fsWebApi } from '@/api/filesystem'
-import { bytesToSize } from '@/utils'
 
 const props = defineProps<{
   appParams: AppParams
@@ -9,16 +8,9 @@ const props = defineProps<{
 
 const emit = defineEmits(['setTitle'])
 
-const SIZE_LIMIT = 100 * 1024 * 1024
+const src = ref('')
 
-const html = ref('')
-const isLoading = ref(false)
-const error = ref('')
-
-async function loadFile() {
-  html.value = ''
-  error.value = ''
-
+watch(() => props.appParams, () => {
   const { appParams } = props
   if (!appParams?.absPath) {
     return
@@ -27,41 +19,17 @@ async function loadFile() {
   const { item, absPath } = appParams
   emit('setTitle', item.name)
 
-  if (item.size != null && item.size > SIZE_LIMIT) {
-    error.value = `File is too large (${bytesToSize(item.size)})`
-    return
-  }
-
-  try {
-    isLoading.value = true
-    html.value = await fsWebApi.stream(absPath, { responseType: 'text' }) as unknown as string
-  }
-  catch (err) {
-    console.error('[HtmlViewer] load failed', err)
-    error.value = 'Failed to load file'
-  }
-  finally {
-    isLoading.value = false
-  }
-}
-
-watch(() => props.appParams, loadFile, { immediate: true })
+  src.value = fsWebApi.getStreamUrl(absPath)
+}, { immediate: true })
 </script>
 
 <template>
   <div class="html-viewer">
-    <div v-if="isLoading" class="html-viewer__status">
-      Loading...
-    </div>
-    <div v-else-if="error" class="html-viewer__status">
-      {{ error }}
-    </div>
     <iframe
-      v-else
+      v-if="src"
       class="html-viewer__frame"
-      sandbox=""
       referrerpolicy="no-referrer"
-      :srcdoc="html"
+      :src="src"
     />
   </div>
 </template>
@@ -70,17 +38,6 @@ watch(() => props.appParams, loadFile, { immediate: true })
 .html-viewer {
   width: 100%;
   height: 100%;
-  // vgo-allow: 外部 HTML 自带样式，容器底色固定为白
-  background-color: #fff;
-
-  &__status {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-    color: var(--vgo-text-secondary);
-  }
 
   &__frame {
     display: block;
